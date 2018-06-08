266b1afbab5b2d2f7bc4919a1de48f0bc74e0958
Steve Longoria <stevehl25@gmail.com>
2018-06-08 14:26:56 -0700

    Set view engine in app.js

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 
 app.use(express.static('public'))
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => res.render(index))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
