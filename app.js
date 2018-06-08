a7d31ce6d6ab3f25e2cb1e07c4fac7329333bb3d
Steve Longoria <stevehl25@gmail.com>
2018-06-08 13:17:11 -0700

    Added pug to show static homepage i.e. plain.html

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static('public'))
 app.get('/', (req, res) => res.render(index))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
-
-app.use(express.static('public'))
