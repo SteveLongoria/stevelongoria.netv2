ecddaf6ef77f58da780825bc6d02b04454296fbf
Steve Longoria <stevehl25@gmail.com>
2018-07-11 11:27:38 -0700

    Added favicon middleware

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var favicon = require('serve-favicon');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// Favicon Setup
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
